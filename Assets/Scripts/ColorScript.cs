c018f798240b6032c376b0685a9fefb1b8e6b33d
LudvigE <aspel25luei@axevalla.fhsk.se>
2026-03-09 15:50:37 +0100

    Color Logic working as intended

--- Assets/Scripts/ColorScript.cs
+++ Assets/Scripts/ColorScript.cs
@@ -4,26 +4,24 @@ using UnityEngine;
 public class ColorScript : MonoBehaviour
 {
     public SpriteRenderer sprite;
-    public Color colorOneTest;
-    public Color colorTwoTest;
+    public Color colorA;
+    public Color colorB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sprite.color = ColorMixer(colorOneTest, colorTwoTest);
+        sprite.color = ColorMixer(colorA, colorB);
     }
 
     public Color ColorMixer(Color colorOne, Color colorTwo)
     {
         Color mixedColor = Color.black;
-          
-
-        mixedColor = colorOne + colorTwo;
-
+        mixedColor = Color.Lerp(colorOne, colorTwo, 0.5f);
         return mixedColor;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        sprite.color = ColorMixer(colorA, colorB);
+
     }
 }
