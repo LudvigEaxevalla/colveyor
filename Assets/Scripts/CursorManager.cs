cabc8fe6f1d56bdb3283f1a16824baed2e51a4e5
LudvigEaxevalla <aspel25luei@axevalla.fhsk.se>
2026-03-16 18:31:56 +0100

    cursor state

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -6,9 +6,80 @@ public class CursorManager : MonoBehaviour
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
-    void Start()
+    public ConveyorBeltScript conveyor;
+
+
+    public enum cursorState
     {
+        Select,
+        Conveyor,
+        Third,
+        Fourth
     }
+
+    private cursorState currentState;
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) TransitionTo(cursorState.Select);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) TransitionTo(cursorState.Conveyor);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) TransitionTo(cursorState.Third);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) TransitionTo(cursorState.Fourth);
+    }
+
+    private void Start()
+    {
+        currentState = cursorState.Select;
+        Debug.Log("Sate is: " + currentState);
+    }
+
+    private void TransitionTo(cursorState newState)
+    {
+        if (currentState == newState) return;
+
+        ExitState(currentState);
+        currentState = newState;
+        EnterState(currentState);
+
+    }
+
+    private void EnterState(cursorState state)
+    {
+        switch (state)
+        {
+            case cursorState.Select:
+                ChangeCursor(0);
+                Debug.Log("Sate is: " + currentState);
+                break;
+
+            case cursorState.Conveyor:
+                ChangeCursor(1);
+                conveyor.isPlacing = true;
+                Debug.Log("Sate is: " + currentState);
+                break;
+
+            case cursorState.Third:
+                ChangeCursor(2);
+                Debug.Log("Sate is: " + currentState);
+                break;
+
+            case cursorState.Fourth:
+                ChangeCursor(3);
+                Debug.Log("Sate is: " + currentState);
+                break;
+        }
+    }
+
+    private void ExitState(cursorState state)
+    {
+        switch (state)
+        {
+            case cursorState.Conveyor:
+                conveyor.isPlacing = false;
+                break;
+        }
+    }
+
 
     public void ChangeCursor(int index)
     {
@@ -16,22 +87,11 @@ public class CursorManager : MonoBehaviour
 
     }
 
+
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ChangeCursor(0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ChangeCursor(1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            ChangeCursor(2);
-        }
+        HandleInput();
 
     }
 }
