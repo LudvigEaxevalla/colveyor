f90ddfe5ca03bb35fd298d9d4b23a88a8d3fbe43
VIP25-git <ht24vipr@axevalla.fhsk.se>
2026-04-13 11:45:38 +0200

    Feature: Color pick

--- Assets/Scripts/Color Picker/SVImageControl.cs
+++ Assets/Scripts/Color Picker/SVImageControl.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ColorPicker : MonoBehaviour
+public class SVImageControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
